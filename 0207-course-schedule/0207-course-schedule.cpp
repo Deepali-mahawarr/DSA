6c6a14563500f263ecc11898e8ac87ee1b18bc72
deepali <deepalimahawar2004@gmail.com>
2026-06-26 13:28:00 +0530

    Time: 11 ms (20.94%), Space: 20.3 MB (17.43%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& edges) {
+         vector<vector<int>>adj(V);
+        vector<int>indeg(V,0);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            
+        }
+        for(int i=0;i<V;i++){
+        for(auto &it:adj[i]){
+            indeg[it]++;
+            
+        }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0)
+            q.push(i);
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            
+            topo.push_back(node);
+            for(auto &it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)
+                q.push(it);
+            }
+
+            
+        }
+        if(topo.size()==V)
+        return true;
+        return false;
+    
+        
+    }
+};
