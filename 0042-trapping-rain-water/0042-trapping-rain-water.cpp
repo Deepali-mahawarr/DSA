406d3a0581dc1110fe3bc1b6131e7468b11a70a4
deepali <deepalimahawar2004@gmail.com>
2026-06-30 20:49:47 +0530

    Time: 0 ms (100%), Space: 26 MB (57.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-vector<int>prefix(vector<int>&arr){
-    int n=arr.size();
-    vector<int>s1(n);
-    s1[0]=arr[0];
-    for(int i=1;i<n;i++){
-        s1[i]=max(s1[i-1],arr[i]);
-    }
-    return s1;
-}
-vector<int>sufix(vector<int>&arr){
-    int n=arr.size();
-    vector<int>s2(n);
-    s2[n-1]=arr[n-1];
-    for(int i=n-2;i>=0;i--){
-        s2[i]=max(s2[i+1],arr[i]);
-    }
-    return s2;
-}
-
     int trap(vector<int>& arr) {
         int n=arr.size();
-        vector<int>pre=prefix(arr);
-        vector<int>suff=sufix(arr);
+        int lmax=0;
+        int rmax=0;
         int total=0;
-        for(int i=0;i<n;i++){
-            int leftmax=pre[i];
-            int rightmax=suff[i];
-            if(arr[i]<leftmax && arr[i]<rightmax)
-            total+=min(leftmax,rightmax)-arr[i];
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(lmax>arr[l]){
+                    total += lmax-arr[l];
+                }
+                else{
+                lmax=arr[l];
+                }
+                l=l+1;
+            }
+            else{
+                 if(rmax>arr[r]){
+                    total += rmax-arr[r];
+                }
+                else{
+                rmax=arr[r];
+                }
+                r=r-1;
 
+            }
         }
         return total;
-
         
     }
 };
