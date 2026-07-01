a76da766b7f993c0b5048996b4cdff8cbd04edc8
deepali <deepalimahawar2004@gmail.com>
2026-07-01 14:24:22 +0530

    Time: 0 ms (100%), Space: 10 MB (97.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,19 +16,39 @@ public:
 //         return solve(n-1,nums,dp);
         
 //     }
- int rob(vector<int>& nums) {
-         int n=nums.size();
-     vector<int>dp(n,0);
-      dp[0]=nums[0];
+//  int rob(vector<int>& nums) {
+//          int n=nums.size();
+//      vector<int>dp(n,0);
+//       dp[0]=nums[0];
+
+//      for(int i=1;i<n;i++){
+//         int pick=nums[i];
+//         if(i>1) pick=nums[i]+dp[i-2];
+//         int notpick=0+dp[i-1];
+//         dp[i]=max(pick,notpick);
+
+//      }
+//      return dp[n-1];
+//  }
+int rob(vector<int>& nums) {
+    int n=nums.size();
+   
+    int prev1=nums[0];
+    int prev2=0;
 
      for(int i=1;i<n;i++){
         int pick=nums[i];
-        if(i>1) pick=nums[i]+dp[i-2];
-        int notpick=0+dp[i-1];
-        dp[i]=max(pick,notpick);
+        if(i>1) pick=nums[i]+prev2;
+        int notpick=0+prev1;
+       
 
+        int curr=max(pick,notpick);
+        prev2=prev1;
+        prev1=curr;
      }
-     return dp[n-1];
- }
+        return prev1;
+
+
+}
      
 };
