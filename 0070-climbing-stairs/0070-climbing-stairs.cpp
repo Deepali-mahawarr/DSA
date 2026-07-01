2ff11c05fdc8e2d61cd11ebbeef8feba93b51c6a
deepali <deepalimahawar2004@gmail.com>
2026-07-01 13:27:47 +0530

    Time: 0 ms (100%), Space: 8.4 MB (50.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-int solve(int ind,vector<int>&dp){
-    if(ind==0 || ind==1) return 1;
-    if(dp[ind]!=-1) return dp[ind];
-    int left=solve(ind-1,dp);
-    int right=INT_MAX;
-    if(right>1) 
-    right=solve(ind-2,dp);
-    // int mini=min(left,right);
-    dp[ind]=left+right;
+// int solve(int ind,vector<int>&dp){
+//     if(ind==0 || ind==1) return 1;
+//     if(dp[ind]!=-1) return dp[ind];
+//     int left=solve(ind-1,dp);
+//     int right=INT_MAX;
+//     if(right>1) 
+//     right=solve(ind-2,dp);
+//     // int mini=min(left,right);
+//     dp[ind]=left+right;
 
-    return dp[ind];
-}
+//     return dp[ind];
+// }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        vector<int>dp(n+1,0);
+         dp[0]=1;
+         dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+
+
+        }
+        return dp[n];
     }
 };
