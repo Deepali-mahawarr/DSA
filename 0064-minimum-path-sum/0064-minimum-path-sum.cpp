2bc25d1ad4e4c60637cae099d43b299cf33bfc0e
deepali <deepalimahawar2004@gmail.com>
2026-07-02 16:00:12 +0530

    Time: 4 ms (14.89%), Space: 16 MB (73.08%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,23 +20,44 @@ public:
 //         return solve(n-1,m-1,mat,dp);
         
 //     }
+    //  int minPathSum(vector<vector<int>>& mat) {
+    //     int n=mat.size();
+    //     int m=mat[0].size();
+    //     vector<vector<int>>dp(n,vector<int>(m,0));
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(i==0 && j==0) dp[i][j]=mat[i][j];
+    //             else{
+    //                 int left=INT_MAX;
+    //                 int up=INT_MAX;
+    //                if(j>0) left=mat[i][j]+dp[i][j-1];
+    //                 if(i>0) up=mat[i][j]+dp[i-1][j];
+    //                  dp[i][j]=min(left,up);
+
+    //             }
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    //  }
      int minPathSum(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
         for(int i=0;i<n;i++){
+             vector<int>curr(m,0);
             for(int j=0;j<m;j++){
-                if(i==0 && j==0) dp[i][j]=mat[i][j];
+                if(i==0 && j==0) curr[j]=mat[i][j];
                 else{
                     int left=INT_MAX;
                     int up=INT_MAX;
-                   if(j>0) left=mat[i][j]+dp[i][j-1];
-                    if(i>0) up=mat[i][j]+dp[i-1][j];
-                     dp[i][j]=min(left,up);
+                   if(j>0) left=mat[i][j]+curr[j-1];
+                    if(i>0) up=mat[i][j]+prev[j];
+                     curr[j]=min(left,up);
 
                 }
             }
+            prev=curr;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
      }
 };
