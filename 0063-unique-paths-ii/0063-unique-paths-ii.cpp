bec27321d83663ca2392100049fc975d592c4176
deepali <deepalimahawar2004@gmail.com>
2026-07-02 15:23:18 +0530

    Time: 0 ms (100%), Space: 11.5 MB (90.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -17,23 +17,46 @@ public:
 
         
 //     }
+    // int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+    //     int n=mat.size();
+    //     int m=mat[0].size();
+    //     vector<vector<int>>dp(n,vector<int>(m,0));
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(mat[i][j]==1) dp[i][j]=0;
+    //             else if(i==0 && j==0) dp[i][j]=1;
+    //             else{
+    //                 int right=0;
+    //                 int down=0;
+    //                 if(j>0) right=dp[i][j-1];
+    //                 if(i>0) down=dp[i-1][j];
+    //                 dp[i][j]=right+down;
+    //             }
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
         for(int i=0;i<n;i++){
+             vector<int>curr(m,0);
             for(int j=0;j<m;j++){
-                if(mat[i][j]==1) dp[i][j]=0;
-                else if(i==0 && j==0) dp[i][j]=1;
+                if(mat[i][j]==1) curr[j]=0;
+                else if(i==0 && j==0) curr[j]=1;
                 else{
                     int right=0;
                     int down=0;
-                    if(j>0) right=dp[i][j-1];
-                    if(i>0) down=dp[i-1][j];
-                    dp[i][j]=right+down;
+                    if(j>0) right=curr[j-1];
+                    if(i>0) down=prev[j];
+                    curr[j]=right+down;
+                    
                 }
             }
+            prev=curr;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
+
 };
