7040cfbb7f20890b6ddbf4acea222111c7a50fe6
deepali <deepalimahawar2004@gmail.com>
2026-07-03 13:52:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (92.46%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -23,28 +23,42 @@ public:
 //     }
 
 
+    //  int minimumTotal(vector<vector<int>>& arr) {
+    //     int n=arr.size();
+    //     vector<vector<int>>dp(n,vector<int>(n,0));
+    //     int i=0;
+    //     for(int j=0;j<n;j++){
+    //             dp[n-1][j]=arr[n-1][j];
+    //         }
+    //     for(int i=n-2;i>=0;i--){
+    //     for(int j=0;j<=i;j++){
+    //     //    
+    //       int right=arr[i][j]+dp[i+1][j+1];
+    //        int down=arr[i][j]+dp[i+1][j];
+    //        dp[i][j]=min(right,down);
+    //     }
+    // }
+    // return dp[0][0];
+    //  }
+
+
      int minimumTotal(vector<vector<int>>& arr) {
         int n=arr.size();
+        vector<int>prev(n,0);
         
-        
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int i=0;
-        
-            for(int j=0;j<n;j++){
-                dp[n-1][j]=arr[n-1][j];
+        for(int j=0;j<n;j++){
+                prev[j]=arr[n-1][j];
             }
-        
-    for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
+            
         for(int j=0;j<=i;j++){
         //     int right=INT_MAX;
         //    int down=INT_MAX;
-          int right=arr[i][j]+dp[i+1][j+1];
-           int down=arr[i][j]+dp[i+1][j];
-           dp[i][j]=min(right,down);
+          int right=arr[i][j]+prev[j+1];
+           int down=arr[i][j]+prev[j];
+           prev[j]=min(right,down);
         }
     }
-    return dp[0][0];
+    return prev[0];
      }
-
-        
 };
