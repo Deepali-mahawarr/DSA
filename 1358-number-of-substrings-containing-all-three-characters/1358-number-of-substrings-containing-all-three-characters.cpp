a377e16e01812532c4cc60d69b62a5601606f54a
deepali <deepalimahawar2004@gmail.com>
2026-07-05 21:02:28 +0530

    Time: 8 ms (52.5%), Space: 11 MB (51.67%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,15 @@
-// class Solution {
-// public:
-//     int numberOfSubstrings(string s) {
-//        int lastSeen[3]={-1,-1,-1};
-//         int cnt=0;
-//         for(int i=0;i<s.size();i++){
-//             lastSeen[s[i]-'a']=i;
-//             if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1)
-//             cnt=cnt + (1+min(lastSeen[0],lastseen[1],lastSeen[2]))
-//         }
-//         return cnt;
-        
-//     }
-// };
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-       vector<int>lastseen(3,-1);
-       int cnt=0;
-       for(int i=0;i<s.size();i++){
-        lastseen[s[i]-'a']=i;
-        if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1)
+        vector<int>lastseen(3,-1);
+        int n=s.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            lastseen[s[i]-'a']=i;
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1)
             cnt=cnt+(1+min(lastseen[0],min(lastseen[1],lastseen[2])));
-}
-       return cnt;
+        }
+        return cnt;
+        
     }
 };
