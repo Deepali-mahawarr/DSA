6f2d1936daabfd16c73e2d814c468e2afc8137f0
deepali <deepalimahawar2004@gmail.com>
2026-07-06 17:05:30 +0530

    Time: 41 ms (93.64%), Space: 21.3 MB (48.2%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int ladderLength(string bw, string ew, vector<string>& wordList) {
+        queue<pair<string,int>>q;
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        q.push({bw,1});
+        st.erase(bw);
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            string word=it.first;
+            int step=it.second;
+            if(word==ew) return step;
+            for(int i=0;i<word.size();i++){
+                int original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        st.erase(word);
+                        q.push({word,step+1});
+                    }
+                }
+                word[i]=original;
+            }
+        }
+        return 0;
+        
+    }
+};
