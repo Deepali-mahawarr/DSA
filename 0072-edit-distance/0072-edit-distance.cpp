cf8988111fdb147c936e149807996a5e313f14b9
deepali <deepalimahawar2004@gmail.com>
2026-07-06 15:02:53 +0530

    Time: 9 ms (38.25%), Space: 13.2 MB (41.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,68 +1,40 @@
 class Solution {
 public:
-    // int f(int i,int j,string &word1,string &word2,vector<vector<int>>&dp){
-    //     if(i<0)
-    //     return j+1; //insert
-    //     if(j<0)
-    //     return i+1;//delete
-    //     if(dp[i][j]!=-1)
-    //     return dp[i][j];
-    //     if(word1[i]==word2[j])
-    //     return dp[i][j]=0+f(i-1,j-1,word1,word2,dp);
-    //     else
-    //     return dp[i][j]=min((1+f(i-1,j-1,word1,word2,dp)),min((1+f(i-1,j,word1,word2,dp)),(1+f(i,j-1,word1,word2,dp))));
+// int solve(int i,int j,string &s1,string &s2,  vector<vector<int>>&dp){
+//     if(i<0) return j+1;
+//     if(j<0) return i+1;
+//     if(s1[i]==s2[j])
+//     return 0+solve(i-1,j-1,s1,s2,dp);
+//     else
+//     return (1+min(solve(i,j-1,s1,s2,dp),min(solve(i-1,j,s1,s2,dp),solve(i-1,j-1,s1,s2,dp))));
 
-    // }
-    // int minDistance(string word1, string word2) {
-    //     int n=word1.size();
-    //     int m=word2.size();
-    //     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-    //     for(int i=0;i<=n;i++) dp[i][0]=i;
-    //     for(int j=0;j<=m;j++) dp[0][j]=j;
-    //     for(int i=1;i<=n;i++){
-    //         for(int j=1;j<=m;j++){
-    //             if(word1[i-1]==word2[j-1]){
-    //                 dp[i][j]=0+dp[i-1][j-1];
-    //             }
-    //             else{
-    //                 dp[i][j]=min((1+dp[i-1][j-1]),min((1+dp[i-1][j]),(1+dp[i][j-1])));
-    //             }
-
-    //         }
-    //     }
-
-    //     return dp[n][m];
+// }
+//     int minDistance(string s1, string s2) {
+//         int n=s1.size();
+//         int m=s2.size();
+//         vector<vector<int>>dp(n,vector<int>(m,-1));
+//         return solve(n-1,m-1,s1,s2,dp);
 
         
-    // }
-    int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<int>prev(m+1,0),curr(m+1,0);
-        
-       // Base case: word1 = ""
-    for(int j = 0; j <= m; j++) {
-        prev[j] = j;
-    }
+//     }
 
-   
 
+int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+        for(int j=0;j<=m;j++)
+        dp[0][j]=j;
         for(int i=1;i<=n;i++){
-            curr[0]=i;
             for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    curr[j]=prev[j-1];
-                }
-                else{
-                    curr[j]=min((1+prev[j-1]),min((1+prev[j]),(1+curr[j-1])));
-                }
-
+                if(s1[i-1]==s2[j-1])
+                dp[i][j]=0+dp[i-1][j-1];
+                else
+                dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
             }
-            prev=curr;
         }
-
-        return prev[m];
-
-        
-    }
+        return dp[n][m];
+}
 };
