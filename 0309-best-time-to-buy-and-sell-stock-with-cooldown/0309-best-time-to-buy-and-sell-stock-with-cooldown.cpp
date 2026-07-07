5cc751014509a124dad936f04c5a462deca6b950
deepali <deepalimahawar2004@gmail.com>
2026-07-07 14:37:11 +0530

    Time: 0 ms (100%), Space: 15.1 MB (79.85%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-int solve(int ind,int buy,vector<int>& prices, vector<vector<int>>&dp){
-    int n=prices.size();
-    long profit=0;
-    if(ind>=n) return 0;
-    if(dp[ind][buy]!=-1) return dp[ind][buy];
-    if(buy){
-        profit=max((-prices[ind]+solve(ind+1,0,prices,dp)),(0+solve(ind+1,1,prices,dp)));
+// int solve(int ind,int buy,vector<int>& prices, vector<vector<int>>&dp){
+//     int n=prices.size();
+//     long profit=0;
+//     if(ind>=n) return 0;
+//     if(dp[ind][buy]!=-1) return dp[ind][buy];
+//     if(buy){
+//         profit=max((-prices[ind]+solve(ind+1,0,prices,dp)),(0+solve(ind+1,1,prices,dp)));
 
-    }
-    else{
-         profit=max((prices[ind]+solve(ind+2,1,prices,dp)),(0+solve(ind+1,0,prices,dp)));
-    }
-    return dp[ind][buy]=profit;
-}
+//     }
+//     else{
+//          profit=max((prices[ind]+solve(ind+2,1,prices,dp)),(0+solve(ind+1,0,prices,dp)));
+//     }
+//     return dp[ind][buy]=profit;
+// }
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//          vector<vector<int>>dp(n,vector<int>(2,-1));
+//          return solve(0,1,prices,dp);
+        
+//     }
+
+
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-         vector<vector<int>>dp(n,vector<int>(2,-1));
-         return solve(0,1,prices,dp);
-        
+        vector<int> front2(2,0),front1(2,0),curr(2,0);
+
+        for(int ind=n-1;ind>=0;ind--){
+
+            curr[1]=max(-prices[ind]+front1[0],0+front1[1]);
+            curr[0]=max(prices[ind]+front2[1],0+front1[0]);
+
+
+            front2=front1;
+            front1=curr;
+        }
+
+        return curr[1];
     }
 };
