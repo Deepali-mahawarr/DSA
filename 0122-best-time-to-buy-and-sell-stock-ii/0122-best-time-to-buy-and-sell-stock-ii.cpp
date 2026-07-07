ac900942414a9a6da4128327453db9e58e48555a
deepali <deepalimahawar2004@gmail.com>
2026-07-07 13:57:43 +0530

    Time: 0 ms (100%), Space: 19.9 MB (97.58%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,26 +21,72 @@ public:
 //         }
 
 
- int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
-        dp[n][0]=0;
-        dp[n][1]=0;
-        long long  profit=0;
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                if(buy==1){
-                  profit=max((-prices[i]+dp[i+1][0]),(0+dp[i+1][1]));
+//  int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<vector<int>>dp(n+1,vector<int>(2,0));
+//         dp[n][0]=0;
+//         dp[n][1]=0;
+//         long long  profit=0;
+//         for(int i=n-1;i>=0;i--){
+//             for(int buy=0;buy<=1;buy++){
+//                 if(buy==1){
+//                   profit=max((-prices[i]+dp[i+1][0]),(0+dp[i+1][1]));
 
-                 }
-                else{
-                     profit=max((prices[i]+dp[i+1][1]),(0+dp[i+1][0]));
+//                  }
+//                 else{
+//                      profit=max((prices[i]+dp[i+1][1]),(0+dp[i+1][0]));
          
-                }
-                dp[i][buy]=profit;
+//                 }
+//                 dp[i][buy]=profit;
 
-            }
-        }
-        return dp[0][1];
- }
+//             }
+//         }
+//         return dp[0][1];
+//     }
+
+
+// int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<int>curr(2,0);
+//         vector<int>ahead(2,0);
+//         ahead[0]=0;
+//         ahead[1]=0;
+//         long long  profit=0;
+//         for(int i=n-1;i>=0;i--){
+//             for(int buy=0;buy<=1;buy++){
+//                 if(buy==1){
+//                   profit=max((-prices[i]+ahead[0]),(0+ahead[1]));
+
+//                  }
+//                 else{
+//                      profit=max((prices[i]+ahead[1]),(0+ahead[0]));
+         
+//                 }
+//                 curr[buy]=profit;
+
+//             }
+//             ahead=curr;
+//         }
+//         return ahead[1];
+//     }
+
+
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        long long anotbuy,abuy,curnotbuy,curbuy;
+        anotbuy=0;
+        abuy=0;
+         for(int i=n-1;i>=0;i--){
+           curbuy=max((-prices[i]+anotbuy),(0+abuy));
+           curnotbuy=max((prices[i]+abuy),(0+anotbuy));
+           abuy=curbuy;
+           anotbuy=curnotbuy;
+         }
+         return abuy;
+    }
+
+
+
+
+
 };
