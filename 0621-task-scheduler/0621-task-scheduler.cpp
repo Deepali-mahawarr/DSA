04a5183d9448e79e4501b47d3dcc725ace63d91c
deepali <deepalimahawar2004@gmail.com>
2026-07-08 21:16:15 +0530

    Time: 41 ms (25.87%), Space: 47.7 MB (20.63%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& arr, int n) {
+        vector<int>mp(26,0);
+        for(char &ch:arr){
+            mp[ch-'A']++;
+        }
+        int time=0;
+        priority_queue<int>pq;
+        for(int i=0;i<26;i++){
+            if(mp[i]>0)
+            pq.push(mp[i]);
+        }
+        while(!pq.empty()){
+            vector<int>temp;
+            for(int i=1;i<=n+1;i++){
+                if(!pq.empty()){
+                    int freq=pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+            for(auto &f:temp){
+                if(f>0)
+                pq.push(f);
+            }
+        if(pq.empty())
+        time+=temp.size();
+        else
+        time=time+(n+1);
+        
+        }
+        return time;
+}
+
+};
