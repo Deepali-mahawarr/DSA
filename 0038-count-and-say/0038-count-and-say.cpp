a98a9c2adcd958bf3ce9d12c7f0b8a759183e936
deepali <deepalimahawar2004@gmail.com>
2026-07-10 17:08:55 +0530

    Time: 1 ms (91.95%), Space: 9.9 MB (45.31%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+    string  say=countAndSay(n-1);
+        string result="";
+        for(int i=0;i<say.size();i++){
+            int cnt=1;
+            char ch=say[i];
+            while(i<say.size() && say[i]==say[i+1]){
+                cnt++;
+                i++;
+            }
+            result+=to_string(cnt)+string(1,ch);
+        }
+        return result;
+        
+    }
+};
