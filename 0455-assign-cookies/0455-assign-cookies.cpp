2352a7303a5f952842eda0e66b7ce0a7c25296a0
deepali <deepalimahawar2004@gmail.com>
2026-07-12 21:37:05 +0530

    Time: 8 ms (58.87%), Space: 44.9 MB (12.1%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int l=0;
+        int r=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while(l<s.size() && r<g.size())
+{
+    if(s[l]>=g[r]){
+        r++;
+    }
+    l++;
+} 
+return r;       
+    }
+};
