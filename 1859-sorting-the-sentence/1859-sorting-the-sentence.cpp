2d8a4dcc2b651479f3c54018411be79a48c8a99b
deepali <deepalimahawar2004@gmail.com>
2026-07-28 15:11:41 +0530

    Time: 3 ms (41.09%), Space: 8.4 MB (87.67%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
     string sortSentence(string s) {
+        int n=s.size();
         vector<string>ans(10);
-        int idx=0;
+        int ind=0;
         int cnt=0;
         string temp;
-        while(idx<s.size()){
-            if(s[idx]==' '){
+        while(ind<s.size()){
+            if(s[ind]==' '){
                 int pos=temp[temp.size()-1]-'0';
                 temp.pop_back();
                 ans[pos]=temp;
                 temp.clear();
-                cnt++;
+                cnt++; 
             }
             else{
-                temp +=s[idx];
+                temp+=s[ind];
             }
-            idx++;
+            ind++;
         }
         int pos=temp[temp.size()-1]-'0';
                 temp.pop_back();
                 ans[pos]=temp;
                 temp.clear();
                 cnt++;
-                
                 for(int i=1;i<=cnt;i++){
                     temp+=ans[i];
                     temp+=' ';
-                
                 }
                 temp.pop_back();
                 return temp;
 
-//        vector<string>ans(10);
-//        string temp;
-//        int cnt=0;
-//        int index=0;
-//        while(index<s.size()){
-//         if(s[index]==' '){
-//             int pos=temp[temp.size()-1]-'0';
-//             temp.pop_back();
-//             ans[pos] = temp;
-//             temp.clear();
-//             cnt++;
-//         }
-//         else{
-//             temp+=s[index];
         
-
-//         }
-//        } 
-//        int pos=temp[temp.size()-1]-'0';
-//             temp.pop_back();
-//             ans[pos]=temp;
-//             temp.clear();
-//             cnt++;
-    
-
-//     for(int i=1;i<=cnt;i++){
-//         temp+=ans[i];
-//         temp +=' ';
-//     }
-// temp.pop_back();
-//     return temp;
     }
 };
