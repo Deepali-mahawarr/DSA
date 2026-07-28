147904c4d4094b1914a7fbf25ea908709a63e042
deepali <deepalimahawar2004@gmail.com>
2026-07-28 14:56:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.59%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]>='a')
+            lower[s[i]-'a']++;
+          else
+            upper[s[i]-'A']++;
+        }
+
+        
+        int cnt=0;
+        int odd=0;
+        for(int i=0;i<26;i++){
+        if(lower[i]%2==0){
+            cnt+=lower[i];
+        }
+        else{
+            cnt +=lower[i]-1;
+            odd=1;
+        }
+        if(upper[i]%2==0){
+            cnt+=upper[i];
+        }
+        else{
+            cnt +=upper[i]-1;
+            odd=1;
+        }
+        }
+        return cnt+odd;
+}
+};
+        
+
+
+
+//    vector<int>lower(26,0);
+    //    vector<int>upper(26,0);
+    //    //freq updated
+    //    for(int i=0;i<s.size();i++){
+    //     if(s[i]>='a'){
+    //         lower[s[i]-'a']++;
+
+    //     }
+    //     else 
+    //     upper[s[i]-'A']++;
+
+    //    } 
+    //    int cnt=0;
+    //    int odd=0;
+    //    for(int i=0;i<26;i++){
+    //     if(lower[i]%2==0){
+    //         cnt+=lower[i];
+            
+    //     }
+    //     else{
+    //      cnt +=lower[i]-1;
+    //         odd=1;
+
+    //     }
+    //      if(upper[i]%2==0){
+    //         cnt+=upper[i];
+            
+    //     }
+    //     else{
+    //         cnt +=upper[i]-1;
+    //         odd=1;
+    //     }
+    //    }
+    //    return cnt+odd;
+
