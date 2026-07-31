bdde18cab363dadd084eb95d72a157da223498c0
deepali <deepalimahawar2004@gmail.com>
2026-07-31 18:24:42 +0530

    Time: 11 ms (22.62%), Space: 15.1 MB (21.42%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void solve(int ind,vector<int>&nums,vector<vector<int>>&ans){
+    if(ind==nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    unordered_set<int>used;
+    for(int i=ind;i<nums.size();i++){
+        if(used.count(nums[i])) continue;
+        used.insert(nums[i]);
+        swap(nums[ind],nums[i]);
+        solve(ind+1,nums,ans);
+        swap(nums[ind],nums[i]);
+    }
+}
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>>ans;
+        solve(0,nums,ans);
+        return ans;
+        
+    }
+};
