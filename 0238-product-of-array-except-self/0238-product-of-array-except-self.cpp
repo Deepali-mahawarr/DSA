a7e85248c1c6f625c6445451dbc340989878df22
deepali <deepalimahawar2004@gmail.com>
2026-08-01 17:26:05 +0530

    Time: 3 ms (37.58%), Space: 42.1 MB (27.28%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,44 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>res(n);
+//         int i=0;
+//         while(i<n){
+//             int self=i;
+//              int ans=1;
+//         for(int j=0;j<n;j++){
+//                 if(j==i) continue;
+//                 ans=ans*nums[j];
+//              }
+//             res[i]=ans;
+//             i++;
+//         }
+//         return res;
+//          }
+// };
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>prefix(n);
+        vector<int>suffix(n);
+        vector<int>res(n);
+        prefix[0]=1;
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+         suffix[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            res[i]=prefix[i]*suffix[i];
+        }
+        return res;
+       
+    }
+};
+
