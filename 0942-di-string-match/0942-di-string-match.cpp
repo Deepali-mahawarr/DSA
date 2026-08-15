265c76a5f8a75e37ad5247b725043da5134c86b0
deepali <deepalimahawar2004@gmail.com>
2026-08-15 16:26:02 +0530

    Time: 0 ms (100%), Space: 11.8 MB (36.85%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n=s.size();
+        vector<int>ans;
+        int low=0;
+        int high=n;
+        for(int i=0;i<n;i++){
+            if(s[i]=='I'){
+                ans.push_back(low);
+                low++;
+            }
+            else{
+                ans.push_back(high);
+                high--;
+            }
+        }
+ans.push_back(low);
+return ans;
+        
+    }
+};
