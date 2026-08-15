2887ed27a93db6505f75523a37593e288a66c1b4
deepali <deepalimahawar2004@gmail.com>
2026-08-15 13:42:31 +0530

    Time: 0 ms (100%), Space: 21.5 MB (97.39%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& arr) {
-       vector<int>st;
-       int n=arr.size();
-       for(int i=0;i<n;i++){
-        if(arr[i]>0)
-        st.push_back(arr[i]);
-        else{
-        while(!st.empty() && st.back()>0 && st.back()<abs(arr[i]))
-        st.pop_back();
-        if(!st.empty() && st.back()==abs(arr[i]))
-        st.pop_back();
-        else if(st.empty() || st.back()<0)
-        st.push_back(arr[i]);
+        int n=arr.size();
+        vector<int>st;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                st.push_back(arr[i]);
+            }
+            else{
+                while(!st.empty() && st.back()>0 && st.back()<abs(arr[i]))
+                {
+                    st.pop_back();
+                }
+                if(!st.empty() && st.back()==abs(arr[i]))
+                st.pop_back();
+                else if(st.empty() || st.back()<0)
+                st.push_back(arr[i]);
+            }
         }
-       }
-       return st;
+        return st;
         
     }
 };
