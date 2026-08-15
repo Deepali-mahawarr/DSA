8a8a07cb41b75408c0370354b2c1b400b6b9136a
deepali <deepalimahawar2004@gmail.com>
2026-08-15 16:46:20 +0530

    Time: 3 ms (82.09%), Space: 13.6 MB (84.1%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,24 +1,45 @@
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         int n=s.size();
+//         stack<char>st;
+//         for(int i=0;i<n;i++){
+//             char ch=s[i];
+//             if(!st.empty() && st.top()==ch){
+//                 st.pop();
+
+//             }
+//            else
+//             st.push(ch);
+//         }
+//         string ans="";
+//         while(!st.empty()){
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+       
+//     }
+// };
+
+
 class Solution {
 public:
     string removeDuplicates(string s) {
         int n=s.size();
-        stack<char>st;
-        for(int i=0;i<n;i++){
-            char ch=s[i];
-            if(!st.empty() && st.top()==ch){
-                st.pop();
-
-            }
-           else
-            st.push(ch);
-        }
         string ans="";
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+
+        // ans=ans+s[0];
+        for(int i=0;i<n;i++){
+            if( !ans.empty() && ans.back()==s[i]){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
         }
-        reverse(ans.begin(),ans.end());
+
         return ans;
-       
     }
 };
