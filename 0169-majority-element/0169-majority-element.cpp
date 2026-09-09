551d3df03a8856e0a4eee8dbfceecac66eda3d17
deepali <deepalimahawar2004@gmail.com>
2026-09-09 18:53:06 +0530

    Time: 0 ms (100%), Space: 28.3 MB (44.27%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=0;
+        int el;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else 
+            cnt--;
+        }
+        int cnt1=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el){
+                cnt1++;
+            }
+        }
+        if(cnt1>=n/2)
+        return el;
+        return -1;
+    }
+};
+
+
+
+
+
+    //     int n=nums.size();
+    //     map<int,int>mpp;
+    //     for(int i=0;i<n;i++){
+    //         mpp[nums[i]]++;
+
+    //     }
+    //     for(auto it:mpp){
+    //         if(it.second>(n)/2){
+    //             return it.first;
+    //         }
+    //     }
+    //   return -1;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int cnt=0;
+        // int el;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(cnt==0){
+        //         cnt=1;
+        //         el=nums[i];
+
+        //     }
+        //     else if(nums[i]==el){
+        //         cnt++;
+        //     }
+        //     else{
+        //         cnt--;
+        //     }
+        // }
+        // int cnt1=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==el){
+        //         cnt1++;
+        //     }
+        //     if(cnt1>(n/2)){
+        //         return el;
+        //     }
+        // }
+        //     return -1;
+    
+        
+        
+    
