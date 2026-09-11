2a76c517b39fa538ec3b7fd2034745f0b5b14c5f
deepali <deepalimahawar2004@gmail.com>
2026-09-11 19:37:25 +0530

    Time: 7 ms (39.43%), Space: 23.7 MB (95.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,58 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
+    vector<vector<int>> merge(vector<vector<int>>&arr) {
+         sort(arr.begin(),arr.end());
         int n=arr.size();
-        sort(arr.begin(),arr.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            if(!ans.empty() && ans.back()[1]>=end) continue; //The previous merged interval already extends beyond the current interval.
-            for(int j=i+1;j<n;j++){
-                if(arr[j][0]<=end){
-                    end=max(end,arr[j][1]);
-
-
-                }
-                else{
-                    break;
-                }
+            if(ans.empty() || arr[i][0] > ans.back()[1]){
+                ans.push_back(arr[i]);
             }
-            ans.push_back({start,end});
+            else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
+            }
         }
         return ans;
+
+        
     }
 };
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         sort(arr.begin(),arr.end());
-//         vector<vector<int>>ans;
-//         for(int i=0;i<arr.size();i++){
-//             if(ans.empty() || arr[i][0]>ans.back()[1]){
-//                 ans.push_back(arr[i]);
-//             }
-//             else{
-//                 ans.back()[1]=max(ans.back()[1],arr[i][1]);
-
-//             }
-//         }
-//         return ans;
-//     }
-// };
