ab65176092a74be7a3df5265dd677c24510bc52e
deepali <deepalimahawar2004@gmail.com>
2026-09-11 15:29:43 +0530

    Time: 33 ms (89.4%), Space: 18.8 MB (71.32%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -9,7 +9,7 @@ static bool comp(string &s1 ,string &s2){
         int first=0;
         int second=0;
         while(first<s1.size()){
-            if(s1[first]==s2[second]){
+            if(s1[first]==s2[second]){ 
             first++;
             second++;
             }
