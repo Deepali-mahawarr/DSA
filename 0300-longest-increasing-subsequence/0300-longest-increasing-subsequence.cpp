4a29a2e0fa19b23b2eb5f2ab52bc879155fffe0e
deepali <deepalimahawar2004@gmail.com>
2026-09-11 12:59:48 +0530

    Time: 2 ms (80.24%), Space: 14 MB (94.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -58,10 +58,11 @@ public:
         int n=nums.size();
         vector<int>temp;
         temp.push_back(nums[0]);
-        
+        int len=1;
         for(int i=1;i<n;i++){
             if(nums[i]>temp.back()){
                 temp.push_back(nums[i]);
+                len++;
             
             }
             else {
@@ -70,7 +71,7 @@ public:
 
             }
         }
-        return temp.size();
+        return len;
      }
 
 };
